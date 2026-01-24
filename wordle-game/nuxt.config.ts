c2744a5cf337356699662846295c0024caa3a0fd
ColtonKnopik <ColtonKnopik@gmail.com>
2026-01-23 18:45:22 -0800

    redo nuxt config

--- wordle-game/nuxt.config.ts
+++ wordle-game/nuxt.config.ts
@@ -1,13 +1,19 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
+    // Compatibility & devtools
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
 
+    // Turn off SSR for static SPA
+    ssr: false,
+
+    // Build settings
     build: {
         transpile: ['vuetify'],
     },
 
+    // Modules
     modules: [
         (_options, nuxt) => {
             nuxt.hooks.hook('vite:extendConfig', (config) => {
@@ -16,6 +22,7 @@ export default defineNuxtConfig({
         },
     ],
 
+    // Vite settings
     vite: {
         vue: {
             template: {
@@ -25,4 +32,10 @@ export default defineNuxtConfig({
     },
 
     css: ['@mdi/font/css/materialdesignicons.css'],
+
+    // Base URL for static assets
+    app: {
+        baseURL: '/',          // root path for deployment
+        buildAssetsDir: '_nuxt/' // default is fine
+    },
 })
