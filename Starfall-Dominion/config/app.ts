653429a905aade30c9067f0b1708652c295f48c1
ColtonKnopik <ColtonKnopik@gmail.com>
2026-02-05 21:40:40 -0800

    increment Version number

--- Starfall-Dominion/config/app.ts
+++ Starfall-Dominion/config/app.ts
@@ -3,7 +3,7 @@
  * Shared constants and configuration values
  */
 
-export const APP_VERSION = '1.0.2'
+export const APP_VERSION = '1.1.2'
 export const APP_NAME = 'STARFALL DOMINION'
 export const APP_TITLE = 'STARFALL'
 export const APP_SUBTITLE = 'DOMINION'
