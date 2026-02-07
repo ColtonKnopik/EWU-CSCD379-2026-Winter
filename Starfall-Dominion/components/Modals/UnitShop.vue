8695738b640c7bf8b8123fa9c2edd617fc4f4137
ColtonKnopik <ColtonKnopik@gmail.com>
2026-02-06 21:17:27 -0800

    Unit shop double click to spawn units

--- Starfall-Dominion/components/Modals/UnitShop.vue
+++ Starfall-Dominion/components/Modals/UnitShop.vue
@@ -12,11 +12,6 @@
             <span class="resource-label">CREDITS</span>
             <span class="resource-value">{{ availableGold }}</span>
           </div>
-          <button @click="$emit('close')" class="close-terminal">
-            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" fill="currentColor"/>
-            </svg>
-          </button>
         </div>
       </div>
 
@@ -39,6 +34,7 @@
                 disabled: availableGold < unit.cost
               }"
               @click="selectUnit(unit)"
+              @dblclick.stop.prevent="purchaseUnit(unit)"
             >
               <div class="item-border"></div>
               <div class="item-content">
