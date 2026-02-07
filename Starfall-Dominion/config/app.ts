24756e69b30c0589ee4bc26c2ac0fccf1018434a
ColtonKnopik <ColtonKnopik@gmail.com>
2026-02-06 18:49:51 -0800

    Corrected Version

--- Starfall-Dominion/config/app.ts
+++ Starfall-Dominion/config/app.ts
@@ -3,7 +3,7 @@
  * Shared constants and configuration values
  */
 
-export const APP_VERSION = '1.3.5-alpha'
+export const APP_VERSION = '2.1.2-beta'
 export const APP_NAME = 'STARFALL DOMINION'
 export const APP_TITLE = 'STARFALL'
 export const APP_SUBTITLE = 'DOMINION'
